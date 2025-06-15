825bee0ccea81b49b4159f683b18efd37cf1814d
UDAY KIRAN <udaykiran.chal2@gmail.com>
2025-06-15 12:40:40 +0530

    Enhance Vite configuration: set base path for deployment, specify build output directory and asset handling, and customize Rollup output file naming conventions.

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/chat-bot/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index.js',
+        chunkFileNames: 'chunk-[hash].js',
+        assetFileNames: 'assets/[hash][extname]'
+      }
+    }
+  }
 })
